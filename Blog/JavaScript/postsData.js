1c61a3196784096121ee5d6ecfb849cc999fb328
KoocheBlacki <152914964+MortezaKoocheBlacki@users.noreply.github.com>
2024-03-07 09:18:10 +0330

    Add files via upload

---
+++ Blog/JavaScript/postsData.js
@@ -0,0 +1,22 @@
+let postInfo = [
+    {
+        postTitle: "مفهوم Dom در JavaScript",
+        postImage: "../images/Images.jpg",
+        postDate: "1399/05/07"
+    },
+    {
+        postTitle: "کار با Grid در CSS",
+        postImage: "../images/Images.jpg",
+        postDate: "1402/05/03"
+    },
+    {
+        postTitle: "BootStrap یا Tailwind",
+        postImage: "../images/Images.jpg",
+        postDate: "1402/06/02"
+    },
+    {
+        postTitle: "آموزش FlexBox",
+        postImage: "../images/Images.jpg",
+        postDate: "1402/03/01"
+    }
+];
